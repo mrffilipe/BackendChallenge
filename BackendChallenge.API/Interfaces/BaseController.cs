1c10aaef127fe1e9d119940dfc638a7269963cf1
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2025-11-03 17:17:40 -0300

    Setting up the project

---
+++ BackendChallenge.API/Interfaces/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendChallenge.API.Interfaces
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+    }
+}
