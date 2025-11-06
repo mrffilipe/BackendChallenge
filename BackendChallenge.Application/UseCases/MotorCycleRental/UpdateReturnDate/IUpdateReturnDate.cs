846800291d45f908bc8c7f3b951d21eaa9a119a8
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2025-11-06 16:28:49 -0300

    Add UpdateReturnDate use case

---
+++ BackendChallenge.Application/UseCases/MotorCycleRental/UpdateReturnDate/IUpdateReturnDate.cs
@@ -0,0 +1,9 @@
+﻿using BackendChallenge.Application.Common;
+
+namespace BackendChallenge.Application.UseCases
+{
+    public interface IUpdateReturnDate
+    {
+        Task<ResponseToTheRequest> ExecuteAsync(string id, UpdateReturnDateDto dto);
+    }
+}
---
+++ BackendChallenge.Application/UseCases/MotorCycleRental/UpdateReturnDate/UpdateReturnDate.cs
@@ -0,0 +1,31 @@
+﻿using BackendChallenge.Application.Common;
+using BackendChallenge.Application.Services;
+
+namespace BackendChallenge.Application.UseCases
+{
+    public class UpdateReturnDate : IUpdateReturnDate
+    {
+        private readonly IMotorcycleRentalQueryRepository _motorcycleRentalQueryRepository;
+        private readonly IUnityOfWorkService _unityOfWorkService;
+
+        public UpdateReturnDate(IMotorcycleRentalQueryRepository motorcycleRentalQueryRepository, IUnityOfWorkService unityOfWorkService)
+        {
+            _motorcycleRentalQueryRepository = motorcycleRentalQueryRepository;
+            _unityOfWorkService = unityOfWorkService;
+        }
+
+        public async Task<ResponseToTheRequest> ExecuteAsync(string id, UpdateReturnDateDto dto)
+        {
+            var result = await _motorcycleRentalQueryRepository.GetByExternalIdAsync(id);
+
+            if (result == null)
+                throw new Exception("Locação com o identificador informado não encontrada.");
+
+            // finalizar a lógica
+
+            await _unityOfWorkService.SaveChangesAsync();
+
+            return new ResponseToTheRequest("Data de devolução informada com sucesso");
+        }
+    }
+}
